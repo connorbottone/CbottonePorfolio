4ab7ca37e86c38c36625c5e54da8c20650650569
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-06 17:36:28 -0500

    added email verification aswell as message length requirments if either 2 arent met a message appears if all sucseful message appers email sent

--- my-app/src/components/Formdata.js
+++ my-app/src/components/Formdata.js
@@ -1,61 +1,139 @@
+// import { useState } from 'react';
+// import { send } from 'emailjs-com';
+// import '../styles/dataform.css'
+// const FormData = () => {
+//     const [toSend, setToSend] = useState({
+//         from_name: '',
+//         message: '',
+//         reply_to: '',
+//       });
+//       const onSubmit = (e) => {
+//         e.preventDefault();
+//         send(
+//           'service_p4lfrx5',
+//           'template_ok240dj',
+//           toSend,
+//           'AGPFhv9zC94kWN0r5'
+//         )
+//           .then((response) => {
+//             console.log('SUCCESS!', response.status, response.text);
+//           })
+//           .catch((err) => {
+//             console.log('FAILED...', err);
+//           });
+//       };
+//       const handleChange = (e) => {
+//         setToSend({ ...toSend, [e.target.name]: e.target.value });
+//       };
+
+
+//     return (
+//         <div className="App">
+//             <form onSubmit={onSubmit}>
+//                 <input
+//                     type='text'
+//                     name='from_name'
+//                     placeholder='from name'
+//                     value={toSend.from_name}
+//                     onChange={handleChange}
+//                 />
+                
+//                 <input
+//                     type='text'
+//                     name='message'
+//                     placeholder='Your message'
+//                     value={toSend.message}
+//                     onChange={handleChange}
+//                 />
+//                 <input
+//                     type='text'
+//                     name='reply_to'
+//                     placeholder='Your email'
+//                     value={toSend.reply_to}
+//                     onChange={handleChange}
+//                 />
+//                 <button type='submit'>S U B M I T</button>
+//             </form>
+//         </div>
+//     );
+// }
 import { useState } from 'react';
 import { send } from 'emailjs-com';
 import '../styles/dataform.css'
+
 const FormData = () => {
-    const [toSend, setToSend] = useState({
-        from_name: '',
-        message: '',
-        reply_to: '',
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    message: '',
+    reply_to: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors({});
+    if (!toSend.reply_to || !/\S+@\S+\.\S+/.test(toSend.reply_to)) {
+      setErrors((prev) => ({ ...prev, reply_to: 'Please enter a valid email address' }));
+      return;
+    }
+
+    if (!toSend.message || toSend.message.length <= 10) {
+      setErrors((prev) => ({ ...prev, message: 'Message should be longer than 10 characters' }));
+      return;
+    }
+
+    send(
+      'service_p4lfrx5',
+      'template_ok240dj',
+      toSend,
+      'AGPFhv9zC94kWN0r5'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setSuccess(true);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
       });
-      const onSubmit = (e) => {
-        e.preventDefault();
-        send(
-          'service_p4lfrx5',
-          'template_ok240dj',
-          toSend,
-          'AGPFhv9zC94kWN0r5'
-        )
-          .then((response) => {
-            console.log('SUCCESS!', response.status, response.text);
-          })
-          .catch((err) => {
-            console.log('FAILED...', err);
-          });
-      };
-      const handleChange = (e) => {
-        setToSend({ ...toSend, [e.target.name]: e.target.value });
-      };
+  };
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
 
-
-    return (
-        <div className="App">
-            <form onSubmit={onSubmit}>
-                <input
-                    type='text'
-                    name='from_name'
-                    placeholder='from name'
-                    value={toSend.from_name}
-                    onChange={handleChange}
-                />
-                
-                <input
-                    type='text'
-                    name='message'
-                    placeholder='Your message'
-                    value={toSend.message}
-                    onChange={handleChange}
-                />
-                <input
-                    type='text'
-                    name='reply_to'
-                    placeholder='Your email'
-                    value={toSend.reply_to}
-                    onChange={handleChange}
-                />
-                <button type='submit'>S U B M I T</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='from_name'
+          placeholder='from name'
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        {errors.reply_to && <div style={{ color: 'red' }}>{errors.reply_to}</div>}
+        <input
+          type='text'
+          name='reply_to'
+          placeholder='Your email'
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        {errors.message && <div style={{ color: 'red' }}>{errors.message}</div>}
+        <input
+          type='text'
+          name='message'
+          placeholder='Your message'
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <button type='submit'>S U B M I T</button>
+        <br>
+        </br>{success && <div style={{ color: 'green' }}>Email sent successfully!</div>}
+      </form>
+      
+    </div>
+    
+  );
 }
-
 export default FormData;
