e803892e675fa6f2e1070ee2a5c03cd59450d4d4
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-06 13:16:12 -0500

    set up add.js to render thru compnonets instead of page changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import { Header, Aboutme, Portfolio, Contactme, Footer, Resume } from './component'
+
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("About");
+
   return (
-    <div className="App">
-    <Main />
-    </div>
+    <>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {currentPage === "About" && <Aboutme />}
+      {currentPage === "Portfolio" && <Portfolio />}
+      {currentPage === "Contact" && <Contactme />}
+      {currentPage === "Resume" && <Resume />}
+      <br></br>
+      <Footer />
+    </>
   );
 }
+
 
 export default App;
