b018ac46763a94bbe7d841e0844d29147c13c4cf
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-07 12:09:06 -0500

    added rusme as pdf that can be downloded

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,8 +1,26 @@
+import  resume from "../public/resume.pdf";
+import React,{useState} from 'react';
+
+
+const handleDownload = () => {
+    window.open(resume);
+  };
+
 const Resume = () => {
     return (
         <div className="resume">
-            <h1>Resume</h1>
-        </div>
+         <object data={resume} type="application/pdf" >
+        <p>
+          It appears your browser doesn't support PDFs. You can download the
+          PDF file{" "}
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            here.
+          </a>
+        </p>
+      </object>
+      <br />
+      <button onClick={handleDownload}>Download PDF</button>
+    </div>
     );
 }
 export default Resume;
