9a1eba25f0b5b126a23dc9e1500582f574671894
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-07 00:20:26 -0500

    modified footer to include indeed.com

--- my-app/src/components/Contactme.js
+++ my-app/src/components/Contactme.js
@@ -14,8 +14,9 @@ const ContactMe = () => {
             <card id='h2'>
                 <h3>CONTACT ME DIRECTLY </h3>
                 
-                <h3>VIA EMAIL &#8594;
-                    </h3>
+                <h3>VIA EMAIL   </h3>
+                <h1>&#8594;</h1>
+                   
                     <h3>MADE USING EMAILJS</h3>
                         </card>
             <FormData />
