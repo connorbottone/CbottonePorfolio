eca3eb8f13338e0e610f84a82b0ecce170955db2
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-06 23:10:10 -0500

    started about me card and add img

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CbottonePorfolio
 
 https://medium.com/geekculture/how-to-send-emails-from-a-form-in-react-emailjs-6cdd21bb4190   emailjs
+
+
+
+https://codepen.io/nashvail/pen/wpGgXO
--- my-app/src/components/Aboutme.js
+++ my-app/src/components/Aboutme.js
@@ -1,10 +1,18 @@
 // import React, { useState } from 'react';
-
+import '../styles/aboutme.css'
+import test from '../images/test.png'
 const AboutMe = () => {
     return (
-        <div>   
-            <h1>About Me</h1>
-            </div>)}
+        <div class="card-outer">
+        <div class="card-inner">
+        <img src={test} alt="Your Image" className="profile-pic" />
+          <div class="about-me">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      )}
             
 
             export default AboutMe;
