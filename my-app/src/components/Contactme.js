9d4f05876f2bf961f3abf756e2309dcaffc5951b
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-06 20:15:53 -0500

    added a card into contact me

--- my-app/src/components/Contactme.js
+++ my-app/src/components/Contactme.js
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import { send } from 'emailjs-com';
 import FormData from "./Formdata";
-
+import'../styles/contactMe.css'
 
 const ContactMe = () => {
 
-
+   
 
     return (
         <div>
-            <h1 id="title">ContactMe Me</h1>
-            <h3  >To contact me directly Via Email fill out the form to the side.
-            </h3>
-            <h3></h3>
-            <h3> This eamil submission was created using email-js</h3>
+            <h1>ContactMe Me</h1>
+            <card id='h2'>
+                <h3>Contact Me Directly Via email  </h3>
+                <h3>by filling out the to the side.
+                    </h3>
+                    <h3>This email sumbission was made using emailjs</h3>
+                        </card>
             <FormData />
-
-
+       
+            
         </div>)
 }
 
